cd15fb34f203cc848c322b3c4121862eff7dbb3b
rinoffi <sivaneshoffi@gmail.com>
2025-07-28 19:38:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -18,14 +18,14 @@ from logging.handlers import RotatingFileHandler
 #rohit_1888 on Tg
 #--------------------------------------------
 #Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "8154426339")
-APP_ID = int(os.environ.get("APP_ID", "")) #Your API ID from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "") #Your API Hash from my.telegram.org
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "8272284753:AAEhKUh12wcEneHAIVnkCEDipXZNZJZmWeo")
+APP_ID = int(os.environ.get("APP_ID", "24160099")) #Your API ID from my.telegram.org
+API_HASH = os.environ.get("API_HASH", "381515f2540b0bf817a68af8b352f5b7") #Your API Hash from my.telegram.org
 #--------------------------------------------
 
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002170811388")) #Your db channel Id
-OWNER = os.environ.get("OWNER", "sewxiy") # Owner username without @
-OWNER_ID = int(os.environ.get("OWNER_ID", "7328629001")) # Owner id
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002469803810")) #Your db channel Id
+OWNER = os.environ.get("OWNER", "Thizz_zoro") # Owner username without @
+OWNER_ID = int(os.environ.get("OWNER_ID", "6248675084")) # Owner id
 #--------------------------------------------
 PORT = os.environ.get("PORT", "8001")
 #--------------------------------------------
