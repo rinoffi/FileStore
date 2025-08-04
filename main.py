8931f7094bd5954bd7416fb221245839fdee127c
rinoffi <sivaneshoffi@gmail.com>
2025-08-04 18:07:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,9 @@ import pyrogram.utils
 
 pyrogram.utils.MIN_CHANNEL_ID = -1009147483647
 
-if __name__ == "__main__":
-    Bot().run()
+if name == "main":
+    # for PTB
+    application.run_polling()
+
+    # or for Pyrogram
+    # app.run()
