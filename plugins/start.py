ff25bb3be480ae6cf20b33297727e6fc74229320
rinoffi <sivaneshoffi@gmail.com>
2025-08-16 13:15:21 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -129,7 +129,7 @@ async def start_command(client: Client, message: Message):
     else:
         reply_markup = InlineKeyboardMarkup(
             [
-                    [InlineKeyboardButton("• ᴍᴏʀᴇ ᴄʜᴀɴɴᴇʟs •", url="https://t.me/Nova_Flix/50")],
+                    [InlineKeyboardButton("• ᴍᴏʀᴇ ᴄʜᴀɴɴᴇʟs •", url="https://t.me/Manga_x_Cruise")],
 
     [
                     InlineKeyboardButton("• ᴀʙᴏᴜᴛ", callback_data = "about"),
